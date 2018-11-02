0a977cf0918bc216479e423a3c7b1f5581b4fbb7
bjaus <brandon.jaus@gmail.com>
2018-11-02 12:06:18 -0500

    removed boilerplate code and added imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+//import $ from 'jquery';
+//import Popper from 'popper.js';
+import 'font-awesome/css/font-awesome.min.css';
+
 import './index.css';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
