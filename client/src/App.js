404b6a4dc4d14285650a3a19f169c669069045a4
bjaus <brandon.jaus@gmail.com>
2018-11-02 22:04:17 -0500

    web token is now persistent

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
 import { Provider } from 'react-redux';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 
 // Redux Store
 import store from './store';
@@ -17,6 +20,16 @@ import Login from './components/auth/Login';
 // Global Styling
 import './App.css';
 
+// Check for Token
+const token = localStorage.jwtToken;
+if ( token ) {
+  // Set auth token header auth
+  setAuthToken(token);
+  // Decode token and get user info and expiration
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
