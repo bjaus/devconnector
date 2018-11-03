dc632f6f5c2a5bb082bd10ef7c63d0bf9c6b992c
bjaus <brandon.jaus@gmail.com>
2018-11-02 22:05:41 -0500

    user can now login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
+
+import { 
+  loginUser 
+} from '../../actions/authActions';
 
 class Login extends Component {
 
@@ -16,6 +23,17 @@ class Login extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    // Check is user is authenticated
+    if ( nextProps.auth.isAuthenticated ) {
+      this.props.history.push('/dashboard');
+    }
+
+    if ( nextProps.errors ) {
+      this.setState({ errors: nextProps.errors });
+    }
+  };
+
   onChange(e) {
     this.setState({
       [e.target.name]: e.target.value
@@ -25,12 +43,12 @@ class Login extends Component {
   onSubmit(e) {
     e.preventDefault();
 
-    const user = {
+    const userData = {
       email: this.state.email,
       password: this.state.password
     };
 
-    console.log(user);
+    this.props.loginUser(userData, this.props.history);
   };
 
   render() {
@@ -43,7 +61,7 @@ class Login extends Component {
             <div className='col-md-8 m-auto'>
               <h1 className='display-4 text-center'>Login</h1>
               <p className='lead text-center'>Sign in to your DevConnector account</p>
-              <form onSubmit={ this.onSubmit }>
+              <form noValidate onSubmit={ this.onSubmit }>
                 <div className='form-group'>
                   <input
                     type='email'
@@ -89,4 +107,15 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect( mapStateToProps, { loginUser } )( withRouter( Login ) );
