aa5783da5ed590cf45629682969d63027c326928
bjaus <brandon.jaus@gmail.com>
2018-11-03 00:15:52 -0500

    logout now clears current profile state

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,11 +4,13 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { logoutUser } from '../../actions/authActions';
+import { clearCurrentProfile } from '../../actions/profileActions';
 
 class Navbar extends Component {
 
   onLogoutClick(e) {
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser(this.props.history);
   }
 
@@ -77,4 +79,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { logoutUser })( withRouter(Navbar) );
+export default connect(mapStateToProps, { logoutUser, clearCurrentProfile })( withRouter(Navbar) );
