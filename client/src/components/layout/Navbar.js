f6ebfcd616dce9bcdc8ab3e4bd39b555dc316075
bjaus <brandon.jaus@gmail.com>
2018-11-02 22:04:40 -0500

    renamed register to sign up

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -20,7 +20,7 @@ class Navbar extends Component {
             </ul>
             <ul className='navbar-nav ml-auto'>
               <li className='nav-item'>
-                <Link to='/register' className='nav-link'>Register</Link>
+                <Link to='/register' className='nav-link'>Sign Up</Link>
               </li>
               <li className='nav-item'>
                 <Link to='/login' className='nav-link'>Login</Link>
