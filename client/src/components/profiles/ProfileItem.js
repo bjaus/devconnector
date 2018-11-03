f024a3a764741e771e35472ad586d8f08d83e116
bjaus <brandon.jaus@gmail.com>
2018-11-03 15:42:56 -0500

    corrected typo; handle not passing to url

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -36,7 +36,7 @@ class ProfileItem extends Component {
             <p>
               { isEmpty(profile.location) ? null : ( <span>{ profile.location }</span> ) }
             </p>
-            <Link to={ `/profile/${ profile.handler }` } className='btn btn-info'>
+            <Link to={ `/profile/${ profile.handle }` } className='btn btn-info'>
               View Profile
             </Link>
           </div>
