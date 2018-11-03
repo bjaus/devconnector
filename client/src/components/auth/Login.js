74582394f0899558671544851752b771c53a36d5
bjaus <brandon.jaus@gmail.com>
2018-11-02 23:27:42 -0500

    now using TextFieldGroup for text fields

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 import { 
   loginUser 
@@ -65,45 +66,36 @@ class Login extends Component {
         <div className='container'>
           <div className='row'>
             <div className='col-md-8 m-auto'>
+
               <h1 className='display-4 text-center'>Login</h1>
               <p className='lead text-center'>Sign in to your DevConnector account</p>
               <form noValidate onSubmit={ this.onSubmit }>
-                <div className='form-group'>
-                  <input
-                    type='email'
-                    className={
-                      classnames('form-control from-control-lg',
-                      {
-                        'is-invalid': errors.email
-                      })
-                    }
-                    placeholder='Email Address'
-                    name='email'
-                    value={ this.state.email }
-                    onChange={ this.onChange }
-                  />
-                { errors.email && ( <div className='invalid-feedback'>{ errors.email }</div> ) }
-                </div>
-                <div className='form-group'>
-                  <input
-                    type='password'
-                    className={
-                      classnames('form-control from-control-lg',
-                      {
-                        'is-invalid': errors.password
-                      })
-                    }
-                    placeholder='Password'
-                    name='password'
-                    value={ this.state.password }
-                    onChange={ this.onChange }
-                  />
-                { errors.password && ( <div className='invalid-feedback'>{ errors.password }</div> ) }
-                </div>
+
+                <TextFieldGroup
+                  placeholder='Email Address'
+                  name='email'
+                  type='email'
+                  value={ this.state.email }
+                  onChange={ this.onChange }
+                  error={ errors.email }
+                />
+
+                <br/>
+
+                <TextFieldGroup
+                  placeholder='Password'
+                  name='password'
+                  type='password'
+                  value={ this.state.password }
+                  onChange={ this.onChange }
+                  error={ errors.email }
+                />
+
                 <input
                   type='submit'
                   className='btn btn-info btn-block mt-4'
                 />
+
               </form>
             </div>
           </div>
