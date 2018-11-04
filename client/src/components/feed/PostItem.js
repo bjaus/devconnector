88383d16e547dcd833e52e7004c4ac2ba1917d55
bjaus <brandon.jaus@gmail.com>
2018-11-04 17:44:45 -0600

    modified post item to be reused in single post view

--- client/src/components/feed/PostItem.js
+++ client/src/components/feed/PostItem.js
@@ -29,7 +29,7 @@ class PostItem extends Component {
   }
 
   render() {
-    const { post, auth } = this.props;
+    const { post, auth, showActions } = this.props;
       
     return (
       <div className='card card-body mb-3'>
@@ -47,38 +47,43 @@ class PostItem extends Component {
           </div>
           <div className='col-md-10'>
             <p className='lead'>{ post.text }</p>
-            <button 
-              onClick={ this.onLikeClick.bind(this, post._id) }
-              type='button' 
-              className='btn btn-light mr-1'
-            >
-              <i className={ classnames('fa fa-thumbs-up', { 
-                'text-info': this.findUserLike(post.likes) 
-              })} />
-              <span className='badge badge-light'>{ post.likes.length }</span>
-            </button>
-            <button 
-              onClick={ this.onUnlikeClick.bind(this, post._id) }
-              type='button' 
-              className='btn btn-light mr-1'
-            >
-              <i className='text-secondary fa fa-thumbs-down' />
-            </button>
-            <Link to={ `/post/${ post._id }` } className='btn btn-info mr-1'>
-              Comments
-            </Link>
-            { post.user === auth.user.id 
-              ? (
-                <button 
-                  onClick={ this.onDeleteClick.bind(this, post._id) } 
-                  type='button' 
-                  className='btn btn-danger mr-1'
+
+          {showActions ? (
+              <span>
+                <button
+                  onClick={ this.onLikeClick.bind(this, post._id) }
+                  type="button"
+                  className="btn btn-light mr-1"
                 >
-                  <i className='fa fa-times' />
+                  <i
+                    className={ classnames('fa fa-thumbs-up', {
+                      'text-info': this.findUserLike(post.likes)
+                    })}
+                  />
+                  <span className="badge badge-light">{ post.likes.length }</span>
                 </button>
-              )
-              : null
-            }
+                <button
+                  onClick={ this.onUnlikeClick.bind(this, post._id) }
+                  type="button"
+                  className="btn btn-light mr-1"
+                >
+                  <i className="text-secondary fa fa-thumbs-down" />
+                </button>
+                <Link to={ `/post/${ post._id }` } className="btn btn-info mr-1">
+                  Comments
+                </Link>
+                { post.user === auth.user.id ? (
+                  <button
+                    onClick={ this.onDeleteClick.bind(this, post._id)}
+                    type="button"
+                    className="btn btn-danger mr-1"
+                  >
+                    <i className="fa fa-times" />
+                  </button>
+                ) : null }
+              </span>
+            ) : null }
+
           </div>
         </div>
       </div>
@@ -86,13 +91,17 @@ class PostItem extends Component {
   }
 }
 
+PostItem.defaultProps = {
+  showActions: true
+};
+
 PostItem.propTypes = {
   deletePost: PropTypes.func.isRequired,
   addLike: PropTypes.func.isRequired,
   removeLike: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   auth: state.auth
