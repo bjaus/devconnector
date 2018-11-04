6e07b4fe01ef0bea5c20e1d7fbae82cacf6ebbdb
bjaus <brandon.jaus@gmail.com>
2018-11-04 14:53:04 -0600

    organized action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,7 +1,19 @@
+// Errors Actions
 export const GET_ERRORS = 'GET_ERRORS';
+
+// Auth Actions
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+
+// Profile Actions
 export const GET_PROFILE = 'GET_PROFILE';
 export const GET_PROFILES = 'GET_PROFILES';
 export const PROFILE_LOADING = 'PROFILE_LOADING';
-export const SET_CURRENT_USER = 'SET_CURRENT_USER';
 export const PROFILE_NOT_FOUND = 'PROFILE_NOT_FOUND';
 export const CLEAR_CURRENT_PROFILE = 'CLEAR_CURRENT_PROFILE';
+
+// Post Actions
+export const POST_LOADING = 'POST_LOADING';
+export const GET_POSTS = 'GET_POSTS';
+export const GET_POST = 'GET_POST';
+export const ADD_POST = 'ADD_POST';
+export const DELETE_POST = 'DELETE_POST';
