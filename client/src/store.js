953d00e536edce9033e3122e2d95887a4c5e0271
bjaus <brandon.jaus@gmail.com>
2018-11-05 12:19:46 -0600

    deploying to heroku

--- client/src/store.js
+++ client/src/store.js
@@ -8,15 +8,7 @@ const middleware = [
   thunk
 ];
 
-//if ( process.env.NODE_ENV === 'production' ) {
-//  const store = createStore(
-//    rootReducer,
-//    initialState,
-//    compose(
-//      applyMiddleware(...middleware)
-//    )
-//  );
-//} else {
+// Redux Chrome Extension
 //const store = createStore(
 //  rootReducer,
 //  initialState,
@@ -25,7 +17,6 @@ const middleware = [
 //    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 //  )
 //);
-//}
 
 const store = createStore(
   rootReducer,
