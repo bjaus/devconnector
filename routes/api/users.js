4784e5380fd79c62baa80654d833fdb8f43930e8
bjaus <brandon.jaus@gmail.com>
2018-11-06 11:40:37 -0600

    reversed sort order for user in develoeprs view

--- routes/api/users.js
+++ routes/api/users.js
@@ -49,8 +49,8 @@ router.get('/all',
               let nameA = a.name.toUpperCase();
               let nameB = b.name.toUpperCase();
 
-              if ( nameA < nameB ) return -1;
-              else if ( nameA > nameB ) return 1;
+              if ( nameA > nameB ) return -1;
+              else if ( nameA < nameB ) return 1;
               else return 0;
             })
           );
